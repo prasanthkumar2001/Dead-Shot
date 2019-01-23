bc2797edeeb543d3f1f4e6bcdbb3f192029621e3
prasanthkumar2001 <42834852+prasanthkumar2001@users.noreply.github.com>
2019-01-23 09:51:25 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,3 @@
-"""
-Simple asteroids clone written in Python
-     _        _                 _     _     
-    / \   ___| |_ ___ _ __ ___ (_) __| |___ 
-   / _ \ / __| __/ _ \ '__/ _ \| |/ _` / __|
-  / ___ \\__ \ ||  __/ | | (_) | | (_| \__ \
- /_/   \_\___/\__\___|_|  \___/|_|\__,_|___/
-by Amin Bandali
 
 """
 from __future__ import division
